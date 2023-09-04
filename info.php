545a8c6c2b97a363290986afcde38a41966d7cff
CDG-c0de <82316777+CDG-c0de@users.noreply.github.com>
2023-09-04 12:35:13 +0200

    Update info.php

--- info.php
+++ info.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     Welcome to the secret santa event (with a twist)!<br><br>
-    Well let's just get right to it. The general concept:<br>
+    The general concept:<br>
     Before partaking please insert your player into the database with the "add your user" button, use your first name please.<br>
     Every participant buys a few CHEAP and preferably funny/obcure gifts, for a total of no more than 15 euros.<br>
     The admin (I) will assign all the gifts to a "starting pile", in the first phase of the game every player gets to<br>
@@ -15,9 +15,7 @@
     ends, and you will be left with the gifts that you have at that moment. If somehow the amount of gifts is significantly<br>
     unbalanced this will be fixed. Gifts do not have to be bought in advance, since Steam does not provide keys.<br>
     You can buy and gift the games to the right person after the event is over, however if you do buy games in advance<br>
-    with a key that is always welcome. This game is a light and fun game, please do not feel pressured for any reason, the gifts<br>
-    can be shit, you will not be assigned specific recipients, it's for shits and giggles.<br><br>
-    All in all, relax and have fun. If there are any questions please feel free to ask them in the discord server.<br><br>
+    with a key that is always welcome.<br><br>
     Merry Chrysler,<br>
     xxx Coen.
 </body>
